340b8255dc39c2be4761548dc29d83038c6acc85
Ilona Sellenberk <ilona.sell@gmail.com>
2023-03-27 16:35:00 +0200

    initial commit

---
+++ Concurrency/Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Ilona Sellenberkova on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
